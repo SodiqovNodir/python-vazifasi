76007b709ca8883207e5d29d4cb70f56987c4f6c
Nodirbek <sodiqovnodirbek14@gmail.com>
2024-11-05 13:33:04 +0500

    malumotlarni soradik

--- main.py
+++ main.py
@@ -8,3 +8,13 @@ print(familya)
 
 yosh = [14, 53, 23]
 print(yosh)
+
+while True:
+    ism = input('Ismingizni kiriting: ')
+    familya = input('Familyangizni kiriting: ')
+    yosh = input('Yoshingizni kiriting: ')
+    tel = input('telefon raqamingizni kiriting: ')
+    elektron_p = input('Email ni kiriting: ')
+    manzil = input('manzilingizni kiriting: ')
+
+
